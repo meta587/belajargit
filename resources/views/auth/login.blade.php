ea1f5f7f5e977c4d701c0a49fe595e75be07c56c
Meta Cahyani <metacahyani4@gmail.com>
2026-08-05 08:44:29 +0700

    Add template &  make layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,69 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@section('title','Login Page - Guestbook')
 
 @section('content')
 <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-lg-6 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                           <div class="col-lg-12">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome To Login Page!</h1>
+                                    </div>
+                                    <form  method="POST" action="{{ route('login') }}" class="user">
+                                        @csrf
+
+                                        <div class="form-group">
+                                            <input type="email" name="email" id="email" class="form-control form-control-user @error('email') is-invalid @enderror"
+                                                value="{{ old('email') }}" placeholder="Enter Your Email Address...">
+                                                @error('email')
+                                                    <div class="invalid-feedback d-block">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password"  name="password" id="password" class="form-control form-control-user @error('password') is-invalid @enderror"
+                                                value="{{ old('password') }}" placeholder="Enter Your Password...">
+                                                @error('password')
+                                                    <div class="invalid-feedback d-block">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" name="remember" id="remember" class="custom-control-input" {{ old('remember') ? 'checked' :  ''}}>
+                                                <label class="custom-control-label" for="remember">Remember Me</label>
+                                            </div>
+                                        </div>
+                                        <button type="sumbit" class="btn btn-primary btn-user btn-block">
+                                            <span class="fa fa-sig-in-alt"></span>login
+                                        </button>
+                                        
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +131,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
---
+++ resources/views/layouts/inc/sidebar.blade.php
@@ -0,0 +1,36 @@
+ <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">GuestBook</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+             <li class="nav-item active">
+                <a class="nav-link" href="">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="">
+                    <i class="fas fa-fw fa-user-alt"></i>
+                    <span>Guest list</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="">
+                    <i class="fas fa-fw fa-user-check-alt"></i>
+                    <span>Employee</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="">
+                    <i class="fas fa-fw fa-user-alt"></i>
+                    <span>Admin</span></a>
+            </li>
+
+        </ul>
